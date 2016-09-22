f3e5f9862adb4f4373ad97eab016fb082fbc18a6
bangvc <bangvc@vgroup.vn>
2016-09-22 17:17:37 +0700

    fix getter

--- src/operators.js
+++ src/operators.js
@@ -1,13 +1,14 @@
-import { Map } from 'immutable';
+import { Map, fromJS } from 'immutable';
 
 export const _stateInit = new Map();
 
 export function _stateIterator(state, callback) {
-  return state.forEach(callback);
+  const listState = Object.keys(state).map(key => state[key]);
+  return listState.forEach(callback);
 }
 
 export function _stateGetter(state, key) {
- return state.get(key);
+ return fromJS(state).get(key);	
 };
 
 export function _stateSetter(state, key, value) {
